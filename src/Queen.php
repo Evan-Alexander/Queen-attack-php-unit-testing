a4afb5cd1f9da685785513b404b4bb2654ad73e8
Jason Brown and Zach Swanson <jason@>
2017-02-13 13:47:33 -0800

    add test cases and functions to check for horizontal and verticl attacks

--- src/Queen.php
+++ src/Queen.php
@@ -2,9 +2,16 @@
 
     class Queen
     {
-        function makeQueenAttack()
+        function makeQueenAttack($queen_position,$attack_position)
         {
-            
+            if ($queen_position[0]==$attack_position[0] || $queen_position[1]==$attack_position[1])
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+
         }
     }
 
--- tests/QueenTest.php
+++ tests/QueenTest.php
@@ -4,21 +4,34 @@
 
     class QueenTest extends PHPUnit_Framework_TestCase
     {
-        function test_QueenAttack()
+        function test_QueenAttackVertical()
         {
             // Arrange
             $test_QueenMove = new Queen;
-            $input;
-
-
+            // Input format: array with Queen Position, other piece position
+            $queen_position = array("C",4);
+            $attack_position = array("C",5);
 
             //Act
-            $result = $test_QueenMove->makeQueenAttack($input);
-
+            $result = $test_QueenMove->makeQueenAttack($queen_position, $attack_position);
 
             //assert
+            $this->assertEquals(true,$result);
+        }
 
-            $this->assertEquals($result);
+        function test_QueenAttackHorizontal()
+        {
+            // Arrange
+            $test_QueenMove = new Queen;
+            // Input format: array with Queen Position, other piece position
+            $queen_position = array("F",4);
+            $attack_position = array("C",4);
+
+            //Act
+            $result = $test_QueenMove->makeQueenAttack($queen_position, $attack_position);
+
+            //assert
+            $this->assertEquals(true,$result);
         }
     }
 
